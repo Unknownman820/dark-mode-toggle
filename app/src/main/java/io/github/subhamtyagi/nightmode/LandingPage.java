7f59f79c685db62537cfb27568b0f3cbeca3b343
Andrew67 <andres@andrew67.com>
2020-02-05 21:01:57 -0500

    Disable radiogroup auto-save Clean up activity code and remove dummy menu item

--- app/src/main/java/io/github/subhamtyagi/nightmode/LandingPage.java
+++ app/src/main/java/io/github/subhamtyagi/nightmode/LandingPage.java
@@ -1,22 +1,16 @@
 package io.github.subhamtyagi.nightmode;
 
 import android.app.UiModeManager;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class LandingPage extends AppCompatActivity {
 
     RadioGroup radioGroup;
-    //RadioButton day, night, auto;
-
     UiModeManager uiModeManager;
 
     @Override
@@ -24,10 +18,7 @@ public class LandingPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         radioGroup = findViewById(R.id.radioGroup);
-        // day = findViewById(R.id.radioDay);
-        // night = findViewById(R.id.radioNight);
-        // auto = findViewById(R.id.radioAuto);
-        uiModeManager = (UiModeManager) getSystemService(Context.UI_MODE_SERVICE);
+        uiModeManager = ContextCompat.getSystemService(this, UiModeManager.class);
 
         switch (uiModeManager.getNightMode()) {
             case UiModeManager.MODE_NIGHT_NO:
@@ -41,44 +32,18 @@ public class LandingPage extends AppCompatActivity {
                 break;
         }
 
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                switch (i) {
-                    case R.id.radioDay:
-                        uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_NO);
-                        break;
-                    case R.id.radioNight:
-                        uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_YES);
-                        break;
-                    case R.id.radioAuto:
-                        uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_AUTO);
-                        break;
-                }
+        radioGroup.setOnCheckedChangeListener((radioGroup, i) -> {
+            switch (i) {
+                case R.id.radioDay:
+                    uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_NO);
+                    break;
+                case R.id.radioNight:
+                    uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_YES);
+                    break;
+                case R.id.radioAuto:
+                    uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_AUTO);
+                    break;
             }
         });
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-
-        }
-
-      /*  if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-            Settings Button.visibility = View.GONE;
-        } else {
-            settings Button.setOnClickListener {
-                startActivityForResult(Intent(android.provider.Settings.ACTION_NIGHT_DISPLAY_SETTINGS), 0)
-            }
-        }*/
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
     }
 }
