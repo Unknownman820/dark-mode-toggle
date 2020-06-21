ca1ae341a4e6c49b2a3b7065526e31b5651dd28e
Andrew67 <andres@andrew67.com>
2020-06-21 13:27:25 -0400

    Show Toast on exception in TileService (fix GPlay ANRs). Set resource configs to en. Update ConstraintLayout.

--- app/src/main/java/com/andrew67/darkmode/QuickTileServices.java
+++ app/src/main/java/com/andrew67/darkmode/QuickTileServices.java
@@ -1,5 +1,6 @@
 package com.andrew67.darkmode;
 
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.UiModeManager;
 import android.content.Intent;
@@ -7,10 +8,14 @@ import android.os.Build;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 @TargetApi(Build.VERSION_CODES.N)
 public class QuickTileServices extends TileService {
     private static final String TAG = "DarkModeQuickTile";
+    private Toast errorToast = null;
 
     @Override
     public void onClick() {
@@ -20,25 +25,37 @@ public class QuickTileServices extends TileService {
             landingPageIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivityAndCollapse(landingPageIntent);
         } else {
-            toggleSetting();
-            syncTile();
+            try {
+                toggleSetting();
+                syncTile();
+            } catch (Exception e) {
+                displayErrorToast();
+            }
         }
     }
 
     @Override
     public void onTileAdded() {
         super.onTileAdded();
-        syncTile();
+        try {
+            syncTile();
+        } catch (Exception e) {
+            displayErrorToast();
+        }
     }
 
     @Override
     public void onStartListening() {
         super.onStartListening();
         if (BuildConfig.DEBUG) Log.d(TAG, "onStartListening");
-        syncTile();
+        try {
+            syncTile();
+        } catch (Exception e) {
+            displayErrorToast();
+        }
     }
 
-    private boolean isNightModeOn(UiModeManager uiModeManager) {
+    private boolean isNightModeOn(@NonNull UiModeManager uiModeManager) {
         final boolean isNightMode = uiModeManager.getNightMode() == UiModeManager.MODE_NIGHT_YES;
         if (BuildConfig.DEBUG) Log.d(TAG, "isNightModeOn " + isNightMode);
         return isNightMode;
@@ -46,24 +63,39 @@ public class QuickTileServices extends TileService {
 
     private void toggleSetting() {
         final UiModeManager uiModeManager = getSystemService(UiModeManager.class);
-        if (isNightModeOn(uiModeManager)) {
-            //switch off the night mode
-            uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_NO);
-            if (BuildConfig.DEBUG) Log.d(TAG, "toggleSetting set nightMode = off");
+        if (uiModeManager != null) {
+            if (isNightModeOn(uiModeManager)) {
+                //switch off the night mode
+                uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_NO);
+                if (BuildConfig.DEBUG) Log.d(TAG, "toggleSetting set nightMode = off");
+            } else {
+                // enable night mode
+                uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_YES);
+                if (BuildConfig.DEBUG) Log.d(TAG, "toggleSetting set nightMode = on");
+            }
         } else {
-            // enable night mode
-            uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_YES);
-            if (BuildConfig.DEBUG) Log.d(TAG, "toggleSetting set nightMode = on");
+            throw new NullPointerException("UiModeManager is null");
         }
     }
 
     private void syncTile() {
         final UiModeManager uiModeManager = getSystemService(UiModeManager.class);
-        final Tile tile = getQsTile();
-        tile.setState(isNightModeOn(uiModeManager) ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
-        tile.updateTile();
-        if (BuildConfig.DEBUG) Log.d(TAG, "syncTile");
+        if (uiModeManager != null) {
+            final Tile tile = getQsTile();
+            tile.setState(isNightModeOn(uiModeManager) ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+            tile.updateTile();
+            if (BuildConfig.DEBUG) Log.d(TAG, "syncTile");
+        } else {
+            throw new NullPointerException("UiModeManager is null");
+        }
     }
 
+    @SuppressLint("ShowToast")
+    private void displayErrorToast() {
+        if (errorToast == null) {
+            errorToast = Toast.makeText(this, R.string.tile_error, Toast.LENGTH_LONG);
+        }
+        errorToast.show();
+    }
 
 }
